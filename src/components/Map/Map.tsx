8f18f3b12d4eb69948c7c8e866e448b72411974b
Jazib Jafri <42325130+JazibJafri@users.noreply.github.com>
2020-09-15 16:48:51 +0500

    add map press handler

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import MapView, { MapViewProps } from 'react-native-maps';
+import MapView, { MapViewProps, MapEvent } from 'react-native-maps';
 
 type MapProps = MapViewProps;
 
 const Map: React.FC<MapProps> = ({ style, region, ...rest }) => {
+    const handleMapPress = (event: MapEvent) => {
+        console.log('Event', event.nativeEvent);
+    };
+
     return (
         <MapView
             style={style}
             showsUserLocation={true}
             showsMyLocationButton={false}
             region={region}
+            onPress={handleMapPress}
             {...rest}
         />
     );
